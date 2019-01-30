53b008896d72571d3d651a1008bb4f44ca23b3b5
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-01-30 11:27:26 +0100

    _showtimes_tests_update

--- moviebase/showtimes/tests.py
+++ moviebase/showtimes/tests.py
@@ -1,30 +1,95 @@
 from rest_framework.test import APITestCase
-from .models import Cinema
+from random import randint, sample
+from .models import Cinema, Screening
+from movielist.models import Person, Movie
+from faker import Faker
+from datetime import datetime, timedelta
+from django.utils import timezone
+
+class Faker_Temp_Data():
+
+    faker = Faker("pl_PL")
+
+    def _fake_cinema_db(self):
+        for _ in range(2):
+            Person.objects.create(name=self.faker.name())
+        for _ in range(2):
+            self._create_fake_movie()
+        m = Movie.objects.all()
+        for _ in range(2):
+            nc = Cinema.objects.create(name='kino ' + self.faker.word(), city=self.faker.city())
+            dt = timezone.make_aware(datetime.today(), timezone.get_current_timezone())
+            Screening.objects.create(cinema=nc, movie=m[0], date=dt + timedelta(randint(1,5)))
+            Screening.objects.create(cinema=nc, movie=m[1], date=dt + timedelta(randint(1,5)))
+
+        Cinema.objects.create(name='kino ' + self.faker.word(), city=self.faker.city())
+
+            # random.randint filmy
+
+    def _random_movie(self):  #po co to???
+        movies = Movie.objects.all()
+        return movies[randint(0, len(movies) - 1)]
+
+    def _random_person(self):
+        """Return a random Person object from db."""
+        people = Person.objects.all()
+        return people[randint(0, len(people) - 1)]
+
+    def _find_person_by_name(self, name):
+        """Return the first `Person` object that matches `name`."""
+        return Person.objects.filter(name=name).first()
+
+    def _fake_movie_data(self):
+        """Generate a dict of movie data
+        The format is compatible with serializers (`Person` relations
+        represented by names).
+        """
+        movie_data = {
+            "title": "{} {}".format(self.faker.job(), self.faker.first_name()),
+            "description": self.faker.sentence(),
+            "year": int(self.faker.year()),
+            "director": self._random_person().name,
+        }
+        people = Person.objects.all()
+        actors = sample(list(people), randint(1, len(people)))
+        actor_names = [a.name for a in actors]
+        movie_data["actors"] = actor_names
+        #print(movie_data["title"])
+        return movie_data
+
+    def _create_fake_movie(self):
+        """Generate new fake movie and save to database."""
+        movie_data = self._fake_movie_data()
+        movie_data["director"] = self._find_person_by_name(movie_data["director"])
+        actors = movie_data["actors"]
+        del movie_data["actors"]
+        new_movie = Movie.objects.create(**movie_data)
+        for actor in actors:
+            new_movie.actors.add(self._find_person_by_name(actor))
+        #return new_movie
 
 
-#import client?
+    def _fake_cinema_json(self):
+        new_cinema = {
+            "name" : "Kino WWW",
+            "city" : "Katowice"
+        }
+
+        return new_cinema
+
+
 
 class CinemaTestCase(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.fake_data = Faker_Temp_Data()
+
     def setUp(self):
         print('Test_1_Cinemas')
-        for i in range(5):
-            Cinema.objects.create(name='Kino {}'.format(i))
-
-    def _fake_cinema(self):
-        new_cinema = {
-            "name" : "Cinema X",
-            "city" : "Katowice"
-        }
-        return new_cinema
-
-    # def _create_new_cinema(self):
-    #     new_cinema_json = self._fake_cinema()
-    #     new_cinema_db = Cinema()
-    #     new_cinema_db.name = new_cinema_json["name"]
-    #     new_cinema_db.city = new_cinema_json["city"]
-    #     new_cinema_db.save()
-
+        CinemaTestCase.fake_data._fake_cinema_db()
+        print(Person.objects.all())
+        print(Movie.objects.all())
 
 
     def test_get_cinemas_list(self):
@@ -37,18 +102,24 @@ class CinemaTestCase(APITestCase):
 
 
     def test_get_cinema_detail(self):
+        c1 = Cinema.objects.get(pk=1)
         response = self.client.get("/cinemas/1/", format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['name'], 'Kino 0')
+        self.assertEqual(response.data['name'], c1.name)
+        self.assertEqual(response.data['city'], c1.city)
         print('detail')
+        print(c1)
+
+
 
     def test_add_cinema(self):
         cinemas_before = Cinema.objects.count()
-        new_cinema = self._fake_cinema()
+        new_cinema = self.fake_data._fake_cinema_json()  #albo nazwa klasy
         response = self.client.post("/cinemas/", new_cinema, format='json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Cinema.objects.count(), cinemas_before + 1)
         print(response.data)
+        print(new_cinema)
         print('add')
         for key, val in new_cinema.items():
             self.assertIn(key, response.data)
@@ -58,8 +129,31 @@ class CinemaTestCase(APITestCase):
             else:
                 self.assertEqual(response.data[key], val)
 
+    def test_delete_cinema(self):
+        response = self.client.delete("/cinemas/1/", {}, format='json')
+        self.assertEqual(response.status_code, 204)
+        cinema_ids = [cinema.pk for cinema in Cinema.objects.all()]
+        self.assertNotIn(1, cinema_ids)
+        print('delete')
 
 
+    def test_update_cinema(self):
+        response = self.client.get("/cinemas/1/", {}, format='json')
+        cinema_data = response.data
+        new_city = 'NYC'
+        cinema_data['city'] = new_city
+        response = self.client.patch("/cinemas/1/", cinema_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        cinema_obj = Cinema.objects.get(id=1)
+        self.assertEqual(cinema_obj.city, new_city)
+        print('update')
+        print(cinema_obj.city)
+
+
+
+
+# class ScreeningTestCase(APITestCase):
+#      pass
 
 
 
