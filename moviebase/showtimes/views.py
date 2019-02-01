0be83921063a140188145c83b2a13a0a321c64c7
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-02-01 16:49:51 +0100

    new_view_added

--- moviebase/showtimes/views.py
+++ moviebase/showtimes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Cinema, Screening
-from .serializers import CinemaSerializer, ScreeningSerializer
+from .serializers import CinemaSerializer, ScreeningSerializer, Movies_30_days_Serializer
 from rest_framework import generics
+#usunac to potem
+from movielist.models import Movie
 
 
 class CinemaView(generics.RetrieveUpdateDestroyAPIView):
@@ -23,3 +25,9 @@ class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
 class ScreeningListView(generics.ListCreateAPIView):
     queryset = Screening.objects.all()
     serializer_class = ScreeningSerializer
+
+
+#dodatkowe
+class CinemaListView30daysMovies(generics.ListAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = Movies_30_days_Serializer
