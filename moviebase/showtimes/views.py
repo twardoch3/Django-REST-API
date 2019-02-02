2121280e4816d14f9cf21a848a05cdac53f50e42
Tomasz Twardoch <ttwardoch3@gmail.com>
2019-02-02 11:56:46 +0100

    views_filters_added

--- moviebase/showtimes/views.py
+++ moviebase/showtimes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Cinema, Screening
-from .serializers import CinemaSerializer, ScreeningSerializer, Movies_30_days_Serializer
-from rest_framework import generics
+from .serializers import CinemaSerializer, ScreeningSerializer, Movies_30_days_Serializer, Movies_30_days_SerializerVersion2
+from rest_framework import generics, filters
 #usunac to potem
 from movielist.models import Movie
 
@@ -14,6 +14,8 @@ class CinemaView(generics.RetrieveUpdateDestroyAPIView):
 class CinemaListView(generics.ListCreateAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('city','name')
 
 
 
@@ -25,9 +27,14 @@ class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
 class ScreeningListView(generics.ListCreateAPIView):
     queryset = Screening.objects.all()
     serializer_class = ScreeningSerializer
+    filter_fields = ('movie__title', 'cinema__city')
 
 
 #dodatkowe
-class CinemaListView30daysMovies(generics.ListAPIView):
+class CinemaListView30daysMovies(generics.ListCreateAPIView):
     queryset = Cinema.objects.all()
     serializer_class = Movies_30_days_Serializer
+
+class CinemaListView30daysMoviesVersion2(generics.ListAPIView):
+    queryset = Cinema.objects.all()
+    serializer_class = Movies_30_days_SerializerVersion2
